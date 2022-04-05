57ff8b7c24cb476345f6a7e5dc7e0183081a9670
Len-En Chiu <linda34264622@gmail.com>
2022-04-05 11:40:33 +0800

    修改Exam Models

--- app/Models/Exam.php
+++ app/Models/Exam.php
@@ -9,6 +9,19 @@ use App\Models\User as Staff;
 class Exam extends Model
 {
     use HasFactory;
+    protected $table = 'exams';
+    protected $fillable = [
+        'id',
+        'product_id',
+        'staff_id',
+        'pass',
+        'perfect',
+        'meet',
+        'start',
+        'end',
+        'date',
+
+    ];
 
     public function staff()
     {
